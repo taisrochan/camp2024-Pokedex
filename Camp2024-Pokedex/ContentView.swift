4d7a690a4272e2ab53f2cb39ca2f640ac1aded96
Tais Rocha <trochanogueira@gmail.com>
2024-05-01 15:19:51 -0300

    subindo icones projeto

--- Camp2024-Pokedex/ContentView.swift
+++ Camp2024-Pokedex/ContentView.swift
@@ -10,18 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+            Image("ioasysIcon")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit) 
+                            .frame(width: 100, height: 100)
             Text("Hello, world!")
                 .font(.custom("Poppins-Regular", size: 16))
         }
         .padding()
-        .background(Color("iOasys"))
     }
     
 }
-//
+
 //#Preview {
 //    ContentView()
 //}
