4f498be5ba49bf8fd444d5af7b88d7269977e4d8
Tais Rocha <trochanogueira@gmail.com>
2024-05-01 10:18:32 -0300

    subindo projeto

---
+++ Camp2024-Pokedex/Camp2024_PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  Camp2024_PokedexApp.swift
+//  Camp2024-Pokedex
+//
+//  Created by Tais Rocha Nogueira on 01/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Camp2024_PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Camp2024-Pokedex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Camp2024-Pokedex
+//
+//  Created by Tais Rocha Nogueira on 01/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
